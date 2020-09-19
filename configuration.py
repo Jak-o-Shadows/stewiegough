aee34ae401892c115f9a688aff2cf1f6b38564bf
Jak_o_Shadows <jak-o-shadows@users.noreply.github.com>
2020-09-19 11:50:11 +0930

    Update configuration to my actual stewart platform

--- configuration.py
+++ configuration.py
@@ -1,44 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 03 14:29:07 2015
-
-@author: Jak
-"""
-import math
+import numpy as np
 
 #define coord system origin as the centre of the bottom plate
 #Find base plate attachment locations
-bAngles = [-5, 5, 115, 125, 235, 245]
-#bAngles = range(0, 360, 360/6)
-bAngles = [math.radians(x) for x in bAngles]
-bR = 50
-bR = 100
-bPos = [[bR*math.cos(theta), bR*math.sin(theta), 0] for theta in bAngles]
+bAngles = np.array([0, 60, 120, 180, 240, 300])
+#bAngles = np.array([180, 300, 60])#, 240])
+bAngles = np.array([-10, 10, 110, 130, 230, 250])
+bAngles = np.radians(bAngles)
+bR = 55/1000
+bPos = np.array([[bR*np.cos(theta), bR*np.sin(theta), 0] for theta in bAngles])
 
-#bPos = [[55, -20, 0], [55, 20, 0], [15, 40, 0], [-55, 20, 0], [-55, -20, 0], [-15, -40, 0]]
+# Define platform 
+#pAngles = 30+np.array([0, 60, 120, 180, 240, 300])
+pAngles =  60+np.roll(np.array([-10, 10, 110, 130, 230, 250]), 1)
+#pAngles = np.roll(np.arange(0, 360, 360/6), 1) + 30
+#pAngles = np.array([180, 0])#, 240])
+pAngles = np.radians(pAngles)
+#pAngles = bAngles# + np.radians(30)
+pR = 52.811/1000
+pPos = np.array([[pR*np.cos(theta), pR*np.sin(theta), 0] for theta in pAngles])
 
-#Platform attachment locations
-#pAngles = [-30, 30, 90, 150, 210, 270]
-#pAngles = range(15, 360+15, 360/6)
-pAngles = [305, 55, 65, 175, 185, 295]
+legYawAngle = bAngles# + np.radians(90)
 
-pAngles = [math.radians(x) for x in pAngles]
-pR = 32
-pR = bR#*0.75
-pPos = [[pR*math.cos(theta), pR*math.sin(theta), 0] for theta in pAngles]
-
-
-
-height = 160
-
-legMin = [50]*6
-legMax = [100]*6
-
-#Base UV joint limits
-A = [math.pi/4]*6
-#Platform ball joint limits
-B = [math.pi/2]*6
-
-#import pprint
-#pprint.pprint(bPos)
-#pprint.pprint(pPos)
+# Define length of legs
+numLegs = pPos.shape[0]
+legLower = np.full(numLegs, 57/1000)
+legUpper = np.full(numLegs, 155/1000)
